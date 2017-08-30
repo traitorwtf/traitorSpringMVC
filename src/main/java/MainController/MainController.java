d872e131fa842343940470c93de5c10f7bd0ee2b
traitorwtf <kkurt@mail.ru>
2017-08-30 14:43:03 +0400

    testing stuff

--- src/main/java/MainController/MainController.java
+++ src/main/java/MainController/MainController.java
@@ -1,17 +1,27 @@
 package MainController;
 
 import Model.User;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.support.RequestContextUtils;
+import org.springframework.web.servlet.view.RedirectView;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.util.Map;
 
 
 @Controller
+@SessionAttributes("userJSP")
 public class MainController {
 
     /*First method on start application*/
@@ -29,26 +39,29 @@ public class MainController {
     то попадем вот сюда
      */
     @RequestMapping(value = "/check-user", method = RequestMethod.POST)
-    public ModelAndView checkUser(@Valid @ModelAttribute("userJSP") User user, BindingResult bindingResult) {
+    public ModelAndView checkUser(@Valid @ModelAttribute("userJSP") User user,
+                                  BindingResult bindingResult) {
         ModelAndView modelAndView = new ModelAndView();
 
         if (bindingResult.hasErrors()){
+
             modelAndView.setViewName("index");
         } else {
-            //имя представления, куда нужно будет перейти
-            modelAndView.setViewName("secondPage");
+            //Применяем модель Post-Redirect-View.
+            RedirectView redirectView = new RedirectView("secondPage");
+            redirectView.setStatusCode(HttpStatus.MOVED_PERMANENTLY);
 
+            modelAndView.setView(redirectView);
         }
         //записываем в атрибут userJSP (используется на странице *.jsp объект user
         modelAndView.addObject("userJSP", user);
 
         return modelAndView; //после уйдем на представление, указанное чуть выше, если оно будет найдено.
+    }
 
-//        if (bindingResult.hasErrors()){
-//            return "index";
-//        }
-//        model.addAttribute("userJSP", user);
-//
-//        return "secondPage";
+    @RequestMapping(value = "/secondPage", method = RequestMethod.GET)
+    public String moveToSecondPage(Model model){
+
+        return "secondPage";
     }
 }
